8a33abe72fca3be95d9018ee875326712e88d16f
kiran-ambalage-18 <78523067+kiran-ambalage-18@users.noreply.github.com>
2021-12-08 14:13:16 +0530

    Added arrays.js file

---
+++ arrays.js
@@ -0,0 +1,14 @@
+
+console.log("Program starts");
+
+let arrayOfNumbers=[10,20,30,40,50];
+
+let length = arrayOfNumbers.length
+
+for (var i=0; i<length; i++ )
+{
+	console.log(i, arrayOfNumbers[i]);
+	
+}
+
+console.log("program ends");
